c4f5931665376692cb68803444c08809ee9b1e51
Maxtoine <bottoni.antoine@gmail.com>
2024-09-14 16:36:18 +0200

    Test commit

--- Adzo-plan/src/components/Box/box.js
+++ Adzo-plan/src/components/Box/box.js
@@ -6,8 +6,6 @@ function Box({ props }) {
     const [x, setX] = useState(props.button.X)
     const [y, setY] = useState(props.button.Y)
     const [id, setId] = useState(props.button.id)
-    
-    console.log(x, y)
 
     return (
         <Text style={[
